a4d5569ac2ce6d75b95daea564e85b745afe145e
Elena Karpovich <elekarpovi@tut.by>
2023-01-27 00:19:06 +0300

    Added task2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear();
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int x;
+
+if (a < 0) a = Math.Abs(a);
+while (a > 0)
+{
+    x = a % 10;
+    sum = sum + x;
+    a = a / 10;
+}
+
+
+Console.WriteLine($"Сумма цифр в числе = {sum}");
